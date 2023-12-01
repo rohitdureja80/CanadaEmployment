93f9184b1861fe70e3414605dd7bff4169ec9e4d
Rohit Dureja <rdureja@gmail.com>
2023-12-01 12:42:05 -0500

    fixing bug where odata was not working for routing / added new nuget package

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ modelBuilder.EntitySet<UnemploymentModel>("Unemployment");
 
 builder.Services.AddSingleton<IDataAccess, SqlDataAccess>();
 
+// allows for odata features 
 builder.Services.AddControllers().AddOData(options => options.Select().Filter()
                 .OrderBy().SetMaxTop(null)
                 .AddRouteComponents(
@@ -36,7 +37,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-//app.UseRouting();
+app.UseRouting();
 app.UseAuthorization();
 //app.UseEndpoints(endpoints => endpoints.MapControllers());
 app.MapControllers();
